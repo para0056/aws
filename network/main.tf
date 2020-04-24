9b2105f105a3ebc5a5afa9012bfe84ae96166a30
nick@nparadis.ca <nick@nparadis.ca>
2020-04-24 15:47:00 -0400

    scaffolding started

---
+++ network/main.tf
@@ -0,0 +1,40 @@
+#* Create VPC *#
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags {
+    Name       = "lse-dev-vpc"
+    CostCentre = "TBD"
+  }
+}
+
+#* Create Subnets *#
+resource "aws_subnet" "app-1a" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ca-central-1a"
+  cidr_block        = var.priv-1a_cidr_block
+}
+
+resource "aws_subnet" "app-1b" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ca-central-1a"
+  cidr_block        = var.priv-1a_cidr_block
+}
+
+resource "aws_subnet" "db-1a" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ca-central-1a"
+  cidr_block        = var.priv-1a_cidr_block
+}
+resource "aws_subnet" "db-1a" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ca-central-1a"
+  cidr_block        = var.priv-1a_cidr_block
+}
+
+#* Create NACLs *#
+
+
---
+++ network/vars.tf
@@ -0,0 +1,7 @@
+variable "vpc_cidr_block" {
+    type = list(string)
+
+
+variable "priv-1a_cidr_block" {
+    type = list(string)
+}
