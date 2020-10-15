411090116437265e2d6fbbd6e01ca49b7d41be8d
Nick Paradis <37662026+para0056@users.noreply.github.com>
2020-10-15 15:46:10 -0400

    Update data.tf

--- guacamole/data.tf
+++ guacamole/data.tf
@@ -5,3 +5,25 @@ data "aws_vpc" "main" {
 data "aws_subnet_ids" "all" {
   vpc_id = data.aws_vpc.main.id
 }
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+
+  filter {
+    name = "owner-alias"
+
+    values = [
+      "amazon",
+    ]
+  }
+}
